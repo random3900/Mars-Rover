775b2697723f637dacb8c93f39a5d9a0b5063695
Roopa Vidhya G <61742380+random3900@users.noreply.github.com>
2020-07-12 00:32:44 +0530

    Update bfs.js

--- bfs.js
+++ bfs.js
@@ -1,8 +1,8 @@
 // C++ program to find the shortest path between
 // a given source cell to a destination cell.
 
-const ROW = 9;
-const COL = 10;
+const ROW = 12;
+const COL = 12;
 
 //To store matrix cell cordinates
 Point = {
@@ -26,190 +26,25 @@ function isValid(row, col) {
 
 // These arrays are used to get row and column
 // numbers of 4 neighbours of a given cell
-rowNum = [-1, 0, 0, 1];
-colNum = [0, -1, 1, 0];
+rowNum = [-1, 0, 0, 1,-1,-1,1,1];
+colNum = [0, -1, 1, 0,-1,+1,-1,+1];
 
 // function to find the shortest path between
 // a given source cell to a destination cell.
 function BFS(mat, src, dest) {
   // check source and destination cell
   // of the matrix have value 1
-  mat[src.x][src.y] = 1;
-  mat[dest.x][dest.y] = 1;
+
 
   console.log(src, dest, mat);
   if (!mat[src.x][src.y] || !mat[dest.x][dest.y]) return -1;
 
-  visited = [
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-    [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ],
-  ];
+  visited = []
+  for(i=0;i<12;i++){
+    visited[i]=[]
+    for(j=0;j<12;j++)
+      visited[i][j]=false
+  }
 
   console.log(visited);
   // Mark the source cell as visited
@@ -228,10 +63,10 @@ function BFS(mat, src, dest) {
     pt = curr.pt;
     if (pt.x == dest.x && pt.y == dest.y) return curr.dist;
     if (!(pt.x == src.x && pt.y == src.y)) {
-      cellNum = pt.y * 12 + pt.x + 1;
+      cellNum = pt.x * 12 + pt.y + 1;
       console.log(cellNum.toString());
       document.getElementById(cellNum.toString()).style.backgroundColor =
-        "lightgreen";
+        "lightblue";
     }
     // If we have reached the destination cell,
     // we are done
@@ -240,7 +75,7 @@ function BFS(mat, src, dest) {
     // and enqueue its adjacent cells
     q.shift();
 
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 8; i++) {
       row = pt.x + rowNum[i];
       col = pt.y + colNum[i];
 
@@ -260,50 +95,30 @@ function BFS(mat, src, dest) {
 }
 
 // Driver program to test above function
-
-// mat = [
-//   [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
-//   [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
-//   [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
-//   [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-//   [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-//   [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
-//   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//   [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
-//   [1, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-// ];
-
-mat = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-];
-// source = { x: 0, y: 0 };
-// dest = { x: 3, y: 4 };
+mat = []
+for(i=0;i<12;i++){
+  mat[i]=[]
+  for(j=0;j<12;j++)
+    mat[i][j]=1
+}
+console.log(mat);
 var v = 0;
 function reply_click(a) {
   if (v == 0) {
     document.getElementById(a).style.backgroundColor = "GREEN";
     v++;
-    source = { x: (a % 12) - 1, y: Math.floor(a / 12) };
+    source = { x:Math.floor((a-1) / 12)  , y: ((a-1) % 12) };
   } else if (v == 1) {
     document.getElementById(a).style.backgroundColor = "RED";
     v++;
-    dest = { x: (a % 12) - 1, y: Math.floor(a / 12) };
+    dest = { x:Math.floor((a-1) / 12)  , y: ((a-1) % 12) };
   } else {
-    document.getElementById(a).style.backgroundColor = "lightblue";
+    document.getElementById(a).style.backgroundColor = "#393e46";
     v++;
-    console.log(Math.floor(a / 12), (a % 12) - 1);
-    mat[(a % 12) - 1][Math.floor(a / 12)] = 0;
+    console.log(Math.floor((a-1) / 12) , ((a-1) % 12));
+    mat[Math.floor((a-1) / 12)][((a-1) % 12) ] = 0;
+    
+    console.log(mat[Math.floor((a-1) / 12)][ ((a-1) % 12)]);
     // dist = BFS(mat, source, dest);
     // if (dist != 10000) console.log("Shortest Path is ", dist);
     // else console.log("Shortest Path doesn't exist");

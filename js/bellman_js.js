8c451c6eccd9b12ddf1d0a277554fe6196880b7b
Roopa Vidhya G <61742380+random3900@users.noreply.github.com>
2020-07-24 23:51:25 +0530

    Update bellman_js.js

--- js/bellman_js.js
+++ js/bellman_js.js
@@ -1,13 +1,12 @@
 //Variables Used
 const ROW = 12;
 const COL = 12;
-const cellColor = "#282828";
-const sourceColor = "GREEN";
+onst cellColor = "#282828";
+const sourceColor = "#76C470";
 const destColor = "#F25050";
-// const blockColor = "#c4f6ff";
-const blockColor = "#393E46";
+const blockColor = "#c4f6ff";
 const traceColor = "#c4fb6d";
-const pathColor = "#BBC86D";
+const pathColor = "#faed27";
 var mat = [];
 var inputType;
 var source = [];
